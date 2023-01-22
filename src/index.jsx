7392328cb1823e352e8d71193ef3aebf3692371d
ArtiomB5 <artiomb5@yandex.by>
2023-01-22 19:37:25 +0600

    refactoring

---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+import './styles/main.scss';
+import React from "react";
+import {App} from "./components/App.jsx"
+import { createRoot } from 'react-dom/client';
+
+const container = document.getElementById('root');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<App tab="home" />);
--- webpack.config.js
+++ webpack.config.js
@@ -1,63 +1,75 @@
-const path = require("path")
+const path = require("path");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin"); // Импортируем плагин
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+// Импортируем плагин
 
-const {CleanWebpackPlugin} = require("clean-webpack-plugin")
-const HTMLWebpackPlugin = require("html-webpack-plugin")
+let mode = "development";
+let target = 'web'; // в режиме разработки browserslist не используется
 
-module.exports = {
-    mode: "development",
-
-    entry: "./src/index.js",
-
-    output: {
-        path: path.resolve(__dirname, "dist"),
-
-        filename: "[name].[hash].js"
-    },
-
-    devServer: {
-        port: 3000,
-    },
-    plugins: [
-        new HTMLWebpackPlugin({template: "./src/index.html"}),
-        new CleanWebpackPlugin()
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.(css|less)$/,
-                use: ["style-loader", "css-loader", "less-loader"]
-            },
-            {
-                test: /\.(jpg|jpeg|png|svg)/,
-                use: ["file-loader"]
-            }
-        ]
-    }
+if (process.env.NODE_ENV === "production") {
+  mode = "production";
 }
 
-// const path = require("path") - импортируется модуль path из Node.js. Этот модуль предоставляет функциональность для работы с путями файловой системы.
+if (process.env.SERVE) { // Используем плагин только если запускаем devServer
+  plugins.push(new ReactRefreshWebpackPlugin());
+} // Данный код должен быть размещен после объявления массива plugins
 
-// const {CleanWebpackPlugin} = require("clean-webpack-plugin") - импортируется класс CleanWebpackPlugin из пакета clean-webpack-plugin. Этот плагин используется для очистки папки с собранными файлами перед каждой сборкой, чтобы избежать дубликатов и ненужных файлов.
+const plugins = [
+  new HtmlWebpackPlugin({
+    template: "./src/index.html", // Данный html будет использован как шаблон
+  }),
+  new MiniCssExtractPlugin(),
+]; // Создаем массив плагинов
 
-// const HTMLWebpackPlugin = require("html-webpack-plugin") - импортируется класс HTMLWebpackPlugin из пакета html-webpack-plugin. Этот плагин используется для генерации HTML файла с нужными скриптами и ссылками, которые нужны для загрузки вашего приложения.
+module.exports = {
+  mode,
+  target,
+  plugins, // Сокращенная запись plugins: plugins в ES6+
+  entry: "./src/index.jsx",
+  devtool: "source-map",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
 
-// module.exports = { - экспортируется конфигурация webpack как объект для дальнейшей настройки.
+  devServer: {
+    hot: true,
+  },
 
-// mode: "development" указывает, что webpack должен использовать режим разработки, который обеспечивает более быструю сборку и более подробные отчеты об ошибках.
-
-// entry: "./src/index.js" указывает, где находится точка входа вашего приложения, которое webpack должен начать собирать.
-
-// output: {path: path.resolve(__dirname, "dist"), filename: "[name].[hash].js"} определяет куда webpack должен записывать собранный код и как называть файл.
-
-// devServer: {port: 3000,} указывает настройки для сервера разработки, который запускается при запуске скрипта npm start. В данном случае задается порт 3000.
-
-// plugins: [new HTMLWebpackPlugin({template: "./src/index.html"}), new CleanWebpackPlugin()] указывает на массив плагинов, которые используются webpack. В данном случае, используются два плагина:
-
-// HTMLWebpackPlugin - этот плагин генерирует HTML файл с нужными скриптами и ссылками, которые нужны для загрузки вашего приложения. Здесь указывается шаблон, который используется для генерации этого файла.
-// CleanWebpackPlugin - этот плагин очищает папку с собранными файлами перед каждой сборкой, чтобы избежать дубликатов и ненужных файлов.
-// module: {rules: [{test: /\.(css|less)$/, use: ["style-loader", "css-loader", "less-loader"]}, {test: /\.(jpg|jpeg|png|svg)/, use: ["file-loader"]}]} это поле указывает на модули и правила обработки файлов. Каждое правило состоит из теста, который проверяет расширение файла и массива загрузчиков (loaders), которые используются для обработки этого типа файлов. В данном случае, файлы css и less обрабатываются с помощью style-loader, `css-loader` и less-loader, а файлы jpg, jpeg, png и svg обрабатываются с помощью file-loader.
-
-// style-loader и css-loader используются для обработки css файлов, они добавляют css в тег head документа, а less-loader используется для обработки less файлов.
-// file-loader используется для обработки файлов изображений, он копирует файл изображения в папку дистрибутива и возвращает путь к файлу.
-
-// Таким образом, этот конфигурационный файл webpack используется для настройки и настройки сборки React приложения, используя webpack. Этот файл определяет точку входа, выходную папку, настройки сервера разработки, плагины и правила обработки файлов.
+  module: {
+    rules: [
+      { test: /\.(html)$/, use: ["html-loader"] }, // Добавляем загрузчик для html
+      {
+        test: /\.(s[ac]|c)ss$/i, // /\.(le|c)ss$/i если вы используете less
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+          'sass-loader',
+        ],
+      }, // Добавляем загрузчики стилей
+      {
+        test: /\.(png|jpe?g|gif|svg|webp|ico)$/i,
+        type: mode === 'production' ? 'asset' : 'asset/resource', // В продакшен режиме
+        // изображения размером до 8кб будут инлайнится в код
+        // В режиме разработки все изображения будут помещаться в dist/assets
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/, // не обрабатываем файлы из node_modules
+        use: {
+          loader: 'babel-loader',
+          options: {
+            cacheDirectory: true, // Использование кэша для избежания рекомпиляции
+            // при каждом запуске
+          },
+        },
+      },
+    ],
+  },
+};
